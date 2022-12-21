538d68fe75f61ed7e948909967a8c37762b45023
ShadeDev7 <shadedev7@gmail.com>
2022-12-21 11:36:28 -0300

    Repository initialization

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main className="flex min-h-screen items-center justify-center">
+            <h1 className="text-center text-4xl font-black">Lineage2 Web</h1>
+        </main>
+    );
+}
