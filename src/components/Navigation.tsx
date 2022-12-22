4433c95986cafb90075d1a84118cbfd384f2ee53
ShadeDev7 <shadedev7@gmail.com>
2022-12-22 20:36:35 -0300

    Add active navigation anchor visualization

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,11 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 import { anchors } from "data";
 
 export default function Navigation() {
+    const { pathname } = useRouter();
+
     return (
         <nav className="overflow-y-overlay flex max-h-[calc(100vh-79px)] flex-col items-center gap-4 pb-3 md:flex-row md:gap-8 md:p-0">
             {anchors.map(({ children, href }, idx) => {
@@ -12,9 +15,9 @@ export default function Navigation() {
                     <Link
                         key={href}
                         href={href}
-                        className={`text-center font-primary text-white hover:underline ${
-                            isLastAnchor ? "" : "after:anchor-separator relative"
-                        }`}
+                        className={`text-center font-primary hover:underline ${
+                            pathname === href ? "text-amber-200" : "text-white"
+                        } ${isLastAnchor ? "" : "after:anchor-separator relative"}`}
                     >
                         {children}
                     </Link>
