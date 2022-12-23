3b0aa7d1c65f54abdcd128fd1266547ffc0faeec
ShadeDev7 <shadedev7@gmail.com>
2022-12-23 16:11:53 -0300

    Add SEO tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import localFont from "@next/font/local";
 
 import "../styles/globals.css";
@@ -14,6 +15,10 @@ const secondaryFont = localFont({
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Head>
+                <title>Lineage 2 NoCry</title>
+            </Head>
+
             <style jsx global>
                 {`
                     :root {
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="es">
-            <Head />
+            <Head>
+                <meta charSet="utf-8" />
+                <meta
+                    name="description"
+                    content="Lineage 2 NoCry es un servidor interlude sub acumulativa base +3 con un gran potencial. ¡Únete a éste nuevo mundo lleno de emocionantes aventuras y escribe tu propia leyenda!"
+                />
+                <meta
+                    name="keywords"
+                    content="Lineage, Lingeage 2, Interlude, Sub Acumulativa, Base +3, Latinoamérica, Sudamérica, Argentina, Bolivia, Brasil, Colombia, Costa Rica, Cuba, Chile, República Dominicana, Ecuador, El Salvador, Guatemala, Honduras, México, Nicaragua, Panamá, Paraguay, Perú, Uruguay, Venezuela"
+                />
+                <meta name="author" content="Agustín Arnoldi" />
+                <meta name="copyright" content="Lineage2NoCry" />
+                <meta name="robots" content="index follow" />
+                <link rel="icon" href="/favicon.png" type="image/png" />
+                <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+            </Head>
 
             <body>
                 <Main />
